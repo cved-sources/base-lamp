bec052ee5397f5206114b75d7031fcd99cefdcac
cved-sources <cved@protonmail.com>
2019-01-31 17:36:19 -0300

    Update main.sh

--- build/main.sh
+++ build/main.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [[ $1 == "default" ]]; then
-    /etc/init.d/mysql start
-    mysqladmin -u root password "root"
-    chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-    mkdir -p /var/lock/apache2 /var/run/apache2 
-    /etc/init.d/apache2 start
-else
-    /etc/init.d/mysql restart
-    /etc/init.d/apache2 restart
-fi
+/etc/init.d/mysql restart
+/etc/init.d/apache2 restart
 
 /usr/bin/tail -f /dev/null
