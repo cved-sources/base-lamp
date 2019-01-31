ff7cc0a36a86f5b8a4f8f0936c1e10e93c4bf986
cved-sources <cved@protonmail.com>
2019-01-31 15:08:17 -0300

    Fix main.sh

--- build/main.sh
+++ build/main.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-
-/etc/init.d/mysql restart
-/etc/init.d/apache2 restart
+if [[ $1 == "default" ]]; then
+    /etc/init.d/mysql start
+    mysqladmin -u root password "root"
+    chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+    mkdir -p /var/lock/apache2 /var/run/apache2 
+    /etc/init.d/apache2 start
+else
+    /etc/init.d/mysql restart
+    /etc/init.d/apache2 restart
+fi
 
 /usr/bin/tail -f /dev/null
